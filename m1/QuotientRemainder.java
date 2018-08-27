dda1a748ff454334cffe3e6b63ab496afc1226ac
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-27 20:26:21 +0530

    HelloWorld.java

---
+++ m1/QuotientRemainder.java
@@ -0,0 +1,11 @@
+public class QuotientRemainder
+{
+	public static void main(String[] args)
+	{
+		int a=10, b=5;
+		int quotient = a/b;
+		int remiander = a%b;
+		System.out.println(quotient+","+remiander);
+
+	}
+}
