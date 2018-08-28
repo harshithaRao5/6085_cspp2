e80b54e57eea7407a41553726a56416dd363f4d5
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-28 19:01:38 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2-assignments/m2/Assignment-2/Assignment-2/Solution.java
+++ CSPP2-assignments/m2/Assignment-2/Assignment-2/Solution.java
@@ -1,31 +1,37 @@
 import java.util.Scanner;
 // import java.lang.Math;
-/*
+/**
 *java code to print the quadractic values.
 */
-public class Solution {
-    /*
-    Do not modify this main function.
+public final class Solution {
+    /**
+    *Do not modify this main function.
+    *@param args string
     */
     public static void main(final String[] args) {
+        /**
+        *main method
+        */
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
         roots(a, b, c);
     }
-    /*
+    /**
     *Need to write the rootsOfQuadraticEquation.
     *function and print the output.
+    *@param a int
+    *@param b int
+    *@param c int
     */
     public static void roots(final int a, final int b, final int c) {
-        /*
-        function to get the results.
+        /**
+        *function to get the results.
         */
         double descriminant = Math.sqrt(b * b - 2 * 2 * a * c);
         double result1 = (-b + descriminant) / (2 * a);
         double result2 = (-b - descriminant) / (2 * a);
         System.out.println(result1 + " " + result2);
-
     }
 }
