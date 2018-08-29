0240ad3256e89432c59962fe8e6b20ff3d25184b
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-29 13:28:56 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignments/m3/Assignment-3/Assignment-3/Solution.java
+++ CSPP2-assignments/m3/Assignment-3/Assignment-3/Solution.java
@@ -1,32 +1,29 @@
 
 import java.util.Scanner;
-/*
-	Do not modify this main function.
+/**
+	*Do not modify this main function.
 	*/
-public class Solution {
-
-    public static void main(String[] args) {
-
+public final class Solution {
+    /**
+    *main function.
+    */
+    public static void main(final String[] args) {
         Scanner s=new Scanner(System.in);      
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        gcd(n1,n2);
+        gcd(n1, n2);
         System.out.println(gcd(n1, n2));
     }
-    /*
-	Need to write the gcd function and print the output.
+    /**
+	*Need to write the gcd function and print the output.
 	*/
-    public static int gcd(int n1, int n2){
-        int gcd=1;
-        for(int i=1;i <= n1 && i <= n2;i++){
-            if(n1 % i == 0 && n2 % i == 0){
+    public static int gcd(final int n1, final int n2) {
+        int gcd = 1;
+        for (int i = 1;i <= n1 && i <= n2;i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
                  gcd = i;
-
-            }
-            
-        }
-        
+            }            
+        }        
          return gcd;
     }
 }
-
