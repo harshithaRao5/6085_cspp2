09922f239f83f864d96d6409b812a5b826c72966
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-30 18:36:14 +0530

    Average.java

---
+++ CSPP2-practice/m4/Average.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Average{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		int sum = 0;
+		for (int i = 0; i<n ; i++){
+			a[i] = sc.nextInt();
+			sum = sum + a[i];
+		}
+		int avg = sum/n;
+		System.out.println(avg);
+
+
+	}
+}
