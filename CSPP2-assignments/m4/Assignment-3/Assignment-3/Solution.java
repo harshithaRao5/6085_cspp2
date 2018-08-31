42f11592256ff173d89d538820b2e2ea242fd3b7
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-31 14:36:01 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- CSPP2-assignments/m4/Assignment-3/Assignment-3/Solution.java
+++ CSPP2-assignments/m4/Assignment-3/Assignment-3/Solution.java
@@ -16,9 +16,9 @@ public final class Solution {
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             String s = sc.next();
-            int res = binaryToDecimal(s);//Write binaryToDecimal function
+            int res = binaryToDecimal(s); //Write binaryToDecimal function
             System.out.println(res);
         }
     }
@@ -29,8 +29,8 @@ public final class Solution {
     */
     public static int binaryToDecimal(final String s) {
         double res = 0;
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '1') {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
                 res = res + Math.pow(2, s.length() - 1 - i);
             }
         }
