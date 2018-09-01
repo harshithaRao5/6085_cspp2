a772f6bd458005825e1e47ee30937a97be0247f1
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-01 11:04:43 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- CSPP2-assignments/m6/Assignment-3/Assignment-3/Solution.java
+++ CSPP2-assignments/m6/Assignment-3/Assignment-3/Solution.java
@@ -31,7 +31,7 @@ final class Solution {
                 charArray[i] = newChar;
             }
         }
-        return charArray;  
+        return charArray;
     }
     /**
      * Main function.
