951bbae264169c0b05064887221fd9f6c833d575
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-03 20:24:12 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignments/m3/Assignment-3/Assignment-3/Solution.java
+++ CSPP2-assignments/m3/Assignment-3/Assignment-3/Solution.java
@@ -16,22 +16,22 @@ public final class Solution {
     */
     public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
-        int n1 = s.nextInt();
-        int n2 = s.nextInt();
-        gcd(n1, n2);
-        System.out.println(gcd(n1, n2));
+        int numberOne = s.nextInt();
+        int numberTwo = s.nextInt();
+        gcd(numberOne, numberTwo);
+        System.out.println(gcd(numberOne, numberTwo));
     }
     /**
     *Need to write the gcd function and print the output.
-    *@param n1 int
-    *@param n2 int
+    *@param numberOne int
+    *@param numberTwo int
     *@return gcd
     */
-    public static int gcd(final int n1, final int n2) {
+    public static int gcd(final int numberOne, final int numberTwo) {
         int gcd = 1;
-        for (int i = 1; i <= n1 && i <= n2; i++) {
-            if (n1 % i == 0 && n2 % i == 0) {
-                 gcd = i;
+        for (int i = 1; i <= numberOne && i <= numberTwo; i++) {
+            if (numberOne % i == 0 && numberTwo % i == 0) {
+                gcd = i;
             }
         }
         return gcd;
