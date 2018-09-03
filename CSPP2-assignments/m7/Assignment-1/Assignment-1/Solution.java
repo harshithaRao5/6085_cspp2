31ac9605bf4ac8fc5392483eea32681aa17fbc74
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-03 19:44:57 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignments/m7/Assignment-1/Assignment-1/Solution.java
+++ CSPP2-assignments/m7/Assignment-1/Assignment-1/Solution.java
@@ -4,12 +4,17 @@ class InputValidator {
 	 * Write the atrributes and methods for InputValidator.
 	 */
 	static String name;
-	public InputValidator(String name) {
+	public InputValidator(final String name) {
 		this.name = name;
 	}
+	/**
+	 * method for checking the length is minimum of 6.
+	 * @return boolean
+	 */
 	public static boolean validateData() {
-		int len = name.length();
-		if (len >= 6) {
+		int namelength = name.length();
+		final int SIX = 6;
+		if (namelength >= SIX) {
 			return true;
 		} else {
 			return false;
@@ -17,11 +22,18 @@ class InputValidator {
 	}
 
 }
-public class Solution {
+public final class Solution {
 	/**
-	 * { item_description }
+	 * default constructor.
 	 */
-	public static void main(String args[]) {
+	private Solution() {
+
+	}
+	/**
+	 * a main class to read input and give output.
+	 * @param args String.
+	 */
+	public static void main(final String args[]) {
 		Scanner s = new Scanner(System.in);
 		String input = s.next();
 		InputValidator i = new InputValidator(input);
