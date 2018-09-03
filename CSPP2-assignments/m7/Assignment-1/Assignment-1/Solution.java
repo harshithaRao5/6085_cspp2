3c70597bd7cada4a18b838cdd1a71e588a88666c
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-03 20:44:30 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignments/m7/Assignment-1/Assignment-1/Solution.java
+++ CSPP2-assignments/m7/Assignment-1/Assignment-1/Solution.java
@@ -1,44 +1,47 @@
 import java.util.*;
 class InputValidator {
-	/**
-	 * Write the atrributes and methods for InputValidator.
-	 */
-	static String name;
-	public InputValidator(final String name) {
-		this.name = name;
-	}
-	/**
-	 * method for checking the length is minimum of 6.
-	 * @return boolean
-	 */
-	public static boolean validateData() {
-		int namelength = name.length();
-		final int SIX = 6;
-		if (namelength >= SIX) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    /**
+     * Write the atrributes and methods for InputValidator.
+     */
+    static String name;
+    public InputValidator(final String name) {
+        this.name = name;
+    }
+    /**
+     * method for checking the length is minimum of 6.
+     * @return boolean
+     */
+    public static boolean validateData() {
+        int namelength = name.length();
+        final int SIX = 6;
+        if (namelength >= SIX) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
+/**
+ * program to check the minimum length of 
+ */
 public final class Solution {
-	/**
-	 * default constructor.
-	 */
-	private Solution() {
+    /**
+     * default constructor.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * a main class to read input and give output.
-	 * @param args String.
-	 */
-	public static void main(final String args[]) {
-		Scanner s = new Scanner(System.in);
-		String input = s.next();
-		InputValidator i = new InputValidator(input);
-		System.out.println(i.validateData());
+    }
+    /**
+     * a main class to read input and give output.
+     * @param args String.
+     */
+    public static void main(final String args[]) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
 
-	}
+    }
 
 }
