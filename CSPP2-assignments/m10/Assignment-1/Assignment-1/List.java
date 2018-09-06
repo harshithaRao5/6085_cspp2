c98332657bb015494970ac5ccfecbc8b3347bb5f
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-06 10:46:55 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- CSPP2-assignments/m10/Assignment-1/Assignment-1/List.java
+++ CSPP2-assignments/m10/Assignment-1/Assignment-1/List.java
@@ -302,11 +302,14 @@ public class List {
         	list[index] = item;
         	size++;
         }
-    	for(int i = size; i > index; i--) {
+        if(index > 0) {
+        	for(int i = size; i > index; i--) {
     		list[i] = list[i-1];
+    		list[index] = item;
+    		size++;
     	}
-    	list[index] = item;
-    	size++;
+        }
+    	
 
     }
     
