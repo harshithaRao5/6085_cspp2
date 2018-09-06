d5886c5a0a8e45af64cb4dba1a71039aea136226
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-06 12:21:16 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- CSPP2-assignments/m10/Assignment-2/Assignment-2/Solution.java
+++ CSPP2-assignments/m10/Assignment-2/Assignment-2/Solution.java
@@ -1,7 +1,7 @@
 import java.io.BufferedInputStream;
 import java.util.Scanner;
 
-public class Solution {
+public final class Solution {
     /**
      * Constructs the object.
      */
@@ -13,10 +13,11 @@ public class Solution {
      * @param args String
      */
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l = (StringListInterface)sl;//Typecasting is done for StringListInterface
+        StringListInterface l = (StringListInterface) sl;
+        //Typecasting is done for StringListInterface
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         // check if there is one more line to process
@@ -31,7 +32,7 @@ public class Solution {
 	                l.add(tokens[1]);
                 break;
                 case "addAll":
-                if (tokens.length == 2){
+                if (tokens.length == 2) {
                 String[] t1 = tokens[1].split(",");
                 l.addAll(t1);
                 }
@@ -61,6 +62,7 @@ public class Solution {
                 case "contains":
                 System.out.println(l.contains(tokens[1]));
                 break;
+                default:
             }
         }
 	}
