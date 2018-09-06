ef4448ca48a6f0ebbc2e06a0c760060c85db58e5
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-06 17:55:51 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- CSPP2-assignments/m10/Assignment-3/Assignment-3/Solution.java
+++ CSPP2-assignments/m10/Assignment-3/Assignment-3/Solution.java
@@ -11,7 +11,7 @@ class Student {
     private String name;
     /**
      * Constructs the object.
-     * @param name String
+     * @param name1 String
      */
     Student(final String name1) {
         //A constructor used to initialize the instance variables
