7b3d72e6dfbe51a58923cc0c2f134519d7076de3
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-15 16:37:02 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

---
+++ CSPP2-assignments/m17/shopping cart/shopping cart/Solution.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public final class Solution {
+
+    private Solution() {
+
+    }
+
+    public static void main(final String[] args) {
+        ShoppingCart sc = new ShoppingCart();
+        Scanner scan = new Scanner(System.in);
+        int testCases = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < testCases; i++) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+                case "Item":
+                String[] c1 = tokens[1].split(",");
+                sc.addToCatalog(new Item(c1[0],
+                    Integer.parseInt(c1[1]), Float.parseFloat(c1[2])));
+
+                break;
+
+                case "catalog":
+                    sc.showCatalog();
+                break;
+
+                case "add":
+                    String[] c2 = tokens[1].split(",");
+                    sc.addToCart(new Item(c2[0],
+                        Integer.parseInt(c2[1]), 0.0f));
+
+                break;
+
+                case "remove":
+                    String[] c3 = tokens[1].split(",");
+                    sc.removeFromCart(new Item(c3[0],
+                        Integer.parseInt(c3[1]), 0.0f));
+
+                break;
+                case "show":
+                    sc.showCart();
+                break;
+                case "totalAmount":
+                    System.out.println("totalAmount: " + sc.getTotalAmount());
+                break;
+                case "payableAmount":
+                    System.out.println("Payable amount: " + sc.
+                        getPayableAmount());
+                break;
+                case "print":
+                    sc.printInvoice();
+                break;
+                case "coupon":
+                    sc.applyCoupon(tokens[1]);
+                break;
+                default:
+                break;
+            }
+
+        }
+    }
+}
