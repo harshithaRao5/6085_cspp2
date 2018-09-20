92f1163fd90c7af1e82dd2a834cb5fa18ae30aaf
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-20 20:08:57 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- CSPP2-assignments/m23/project/Assignment-1/Assignment-1/Solution.java
+++ CSPP2-assignments/m23/project/Assignment-1/Assignment-1/Solution.java
@@ -93,15 +93,15 @@ class Solution {
 				result[i][j] = Frequency.similarity(Frequency.toString(listoffiles[i]),Frequency.toString(listoffiles[j]));
 			}
 		}
-		System.out.println("\t      ");
+		System.out.println("      \t");
 		for (int i = 0; i < length; i++) {
 			System.out.print("\t" + listoffiles[i].getName());
 		}
 		System.out.println();
 		for (int i = 0; i < length; i++) {
-			System.out.print(listoffiles[i].getName());
+			System.out.print(listoffiles[i].getName() + "\t");
 			for (int j = 0; j < length; j++) {
-				System.out.print("\t" + result[i][j] + "\t");
+				System.out.print(result[i][j] + "\t");
 			}
 			System.out.println();
 		}
