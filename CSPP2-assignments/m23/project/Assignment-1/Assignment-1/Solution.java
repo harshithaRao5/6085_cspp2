1095002629a40832044b4e69ba2015209493f8e0
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-20 23:02:18 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- CSPP2-assignments/m23/project/Assignment-1/Assignment-1/Solution.java
+++ CSPP2-assignments/m23/project/Assignment-1/Assignment-1/Solution.java
@@ -17,14 +17,14 @@ class Frequency {
 			}
 			input.close();
 			s = sb.toString();
-		}catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			System.out.println("no file");
 		}
 		return s;
 	}
 	public static Map removeAll(String text) {
-		String wordList1 = text.replaceAll("[^a-zA-Z0-9 ]","");
-		String[] wordList = wordList1.split(" ");
+		String[] wordList = text.replaceAll("[^a-zA-Z0-9 ]","").split(" ");
+
 		//System.out.println(Arrays.toString(wordList1));
 		//return Arrays.toString(wordList1);
 		Map<String, Integer> map = new HashMap<>();
@@ -44,7 +44,6 @@ class Frequency {
 	public static int similarity(String doc1, String doc2) {
 		//System.out.println("hello");
 		double numerator = 0;
-		double denominator = 0;
 		double sum1 = 0;
 		double sum2 = 0;
 		Map<String, Integer> mapOne = removeAll(doc1);
@@ -54,7 +53,6 @@ class Frequency {
 				if (element1.equals(element2)){
 					numerator += mapOne.get(element1)*mapTwo.get(element2);
 				}
-
 			}
 		}
 		for (String element1:mapOne.keySet()) {
@@ -63,13 +61,12 @@ class Frequency {
 		for (String element2:mapTwo.keySet()) {
 			sum2 += Math.pow(mapTwo.get(element2), 2);
 		}
-		denominator = Math.sqrt(sum1) * Math.sqrt(sum2);
+		double denominator = Math.sqrt(sum1) * Math.sqrt(sum2);
 		// System.out.println(sum1);
 		// System.out.println(sum2);
 		// System.out.println(numerator);
 		// System.out.println(denominator);
-		double result = ((numerator/denominator)*100D)/100D;
-		return (int)(result*100);
+		return (int)((((numerator/denominator)*100D)/100D)*100);
 
 
 	}
@@ -80,8 +77,8 @@ class Solution {
 		try {
 		Frequency f = new Frequency();
 		Scanner sc = new Scanner(System.in);
-		String line = sc.next();
-		File input = new File(line);
+		// String line = sc.next();
+		File input = new File(sc.next());
 		File[] listoffiles = input.listFiles();
 		// for (File name:listoffiles) {
 		// 	//System.out.println(name);
