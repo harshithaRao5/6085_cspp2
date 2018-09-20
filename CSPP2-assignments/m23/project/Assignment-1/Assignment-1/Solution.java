965749b1cc0ed70f59bb7b733d8783fd80730d5f
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-20 20:01:37 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- CSPP2-assignments/m23/project/Assignment-1/Assignment-1/Solution.java
+++ CSPP2-assignments/m23/project/Assignment-1/Assignment-1/Solution.java
@@ -101,7 +101,7 @@ class Solution {
 		for (int i = 0; i < length; i++) {
 			System.out.print(listoffiles[i].getName());
 			for (int j = 0; j < length; j++) {
-				System.out.print("\t"+result[i][j]);
+				System.out.print("\t" + result[i][j] + "\t");
 			}
 			System.out.println();
 		}
