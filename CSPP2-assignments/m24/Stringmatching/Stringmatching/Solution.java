20427e0121194e13bb8f7abb41846f57f927e770
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-23 10:52:04 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- CSPP2-assignments/m24/Stringmatching/Stringmatching/Solution.java
+++ CSPP2-assignments/m24/Stringmatching/Stringmatching/Solution.java
@@ -100,7 +100,7 @@ class Solution {
 			}
 			}
 		}
-		System.out.print("    ");
+		System.out.print("      \t");
 		for (int i = 0; i < length; i++) {
 			System.out.print("\t" + listoffiles[i].getName());
 		}
